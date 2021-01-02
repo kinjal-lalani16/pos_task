a72c8119acc4323910e63561a0b3c117b7bd23a7
Kinal <kinjal.lalani@icreativetechnolabs.com>
2021-01-02 16:56:54 +0530

    [Init]Initial commit.

---
+++ pos_task/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pos_task",
+    'summary': """This module will give message through pop up that customer
+                    details does not contain zipcode in pos.""",
+    'description': """This module will give message through pop up that customer
+                    details does not contain zipcode in pos.""",
+    'author': "Aktiv software",
+    'website': "http://www.aktivsoftware.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.0',
+    'depends': ['point_of_sale'],
+    'data': [
+
+    ],
+}
